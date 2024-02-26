4c44588fe7513955322a6ddc8017303b3a52f5a3
Tatuuz <151540589+Tatuuz@users.noreply.github.com>
2024-02-26 15:01:57 +0100

    Create le tabelle
    Create ed eseguite le tabelle con le proprie chiavi primarie ed esterne

--- Database/tabelle.sql
+++ Database/tabelle.sql
@@ -1,7 +1,7 @@
 DROP SCHEMA IF EXISTS a CASCADE;
 CREATE SCHEMA a;
 
---creazione tabelle
+--CREAZIONE TABELLE
 
 CREATE TABLE a.FAMIGLIA(
     NomeGruppo varchar(32),
@@ -26,80 +26,96 @@ CREATE TABLE a.UTENTE(
     CONSTRAINT UK_Utente UNIQUE (Username)
 );
 
-CREATE TABLE a.CONTO(
-    IBAN varchar(27),
-    SaldoConto float,
-    CodF varchar(16),
-
-    CONSTRAINT PK_Conto PRIMARY KEY (IBAN),
-    CONSTRAINT FK_Utente FOREIGN KEY(CodF) REFERENCES a.UTENTE(CodF) ON DELETE CASCADE
-);
-
-CREATE TABLE a.SPESE_PROGRAMMATE (
-    Descrizione varchar(64),
-    Periodicita varchar(16),
-    DataScdenza date,
-    IBAN varchar(27),
-
-    CONSTRAINT FK_Conto FOREIGN KEY(IBAN) REFERENCES a.CONTO(IBAN) ON DELETE CASCADE
-);
-
 CREATE TABLE a.CARTA_DEBITO (
-    NumeroCarta INTEGER,
-    Scadenza date,
-    PIN SMALLINT,
-    CVV SMALLINT,
+    id_Carta serial,
+    nome_carta varchar(32),
+    Scadenza date NOT NULL,
     SaldoCarta float,
-    IBAN varchar(27),
+    cf_utente varchar(16),
+    --IBAN varchar(27),
 
-    CONSTRAINT PK_CARTA_DEBITO PRIMARY KEY (NumeroCarta),
-    CONSTRAINT FK_Conto FOREIGN KEY(IBAN) REFERENCES a.CONTO(IBAN) ON DELETE CASCADE
+    CONSTRAINT PK_CARTA_DEBITO PRIMARY KEY (id_Carta),
+    CONSTRAINT FK_UTENTE FOREIGN KEY(cf_utente) REFERENCES a.UTENTE(CodF) ON DELETE CASCADE
+    --CONSTRAINT FK_Conto FOREIGN KEY(IBAN) REFERENCES a.CONTO(IBAN) ON DELETE CASCADE
 
 );
 
 CREATE TABLE a.CARTA_CREDITO (
-    NumeroCarta INTEGER,
-    Scdenza date,
-    PIN SMALLINT,
-    CVV SMALLINT,
+    id_carta serial,
+    Nome_Carta varchar(32),
+    Scdenza date NOT NULL,
     plafond float,
-    IBAN varchar(27),
+    cf_utente varchar(16),
+    --IBAN varchar(27),
 
-    CONSTRAINT PK_CARTA_CREDTO PRIMARY KEY (NumeroCarta),
-    CONSTRAINT FK_Conto FOREIGN KEY(IBAN) REFERENCES a.CONTO(IBAN) ON DELETE CASCADE
+    CONSTRAINT PK_CARTA_CREDTO PRIMARY KEY (id_carta),
+    CONSTRAINT FK_UTENTE FOREIGN KEY(cf_utente) REFERENCES a.UTENTE(CodF) ON DELETE CASCADE
+    --CONSTRAINT FK_Conto FOREIGN KEY(IBAN) REFERENCES a.CONTO(IBAN) ON DELETE CASCADE
+);
+
+CREATE TABLE a.SPESE_PROGRAMMATE (
+    id_Spesa SERIAL,
+    Descrizione varchar(64),
+    Periodicita varchar(16),
+    DataScdenza date,
+    CartaDebito INTEGER,
+    CartaCredito INTEGER,
+    --IBAN varchar(27),
+
+     CONSTRAINT PK_SPESA PRIMARY KEY (id_Spesa),
+    CONSTRAINT FK_CARTA_CREDITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_CREDITO(id_carta) ON DELETE CASCADE,
+    CONSTRAINT FK_CARTA_DEBITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_DEBITO(id_Carta) ON DELETE CASCADE
+    --CONSTRAINT FK_Conto FOREIGN KEY(IBAN) REFERENCES a.CONTO(IBAN) ON DELETE CASCADE
+);
+
+
+CREATE TABLE a.PORTAFOGLIO(
+    id_Portafoglio serial,
+    nome_portafoglio varchar(32),
+    saldo float NOT NULL,
+
+    CONSTRAINT PK_PORTAFOGLIO PRIMARY KEY (id_Portafoglio)
 );
 
 CREATE TABLE a.TRANSAZIONE_ENTRATA(
-    CRO varchar(11),
+    CodTransazione serial,
     Importo float,
     DataTransazione date NOT NULL,
-    Casuale varchar(64),
-    Mittente varchar(128) NOT NULL,
     Categoria varchar(16) NOT NULL,
     CartaDebito INTEGER,
     CartaCredito INTEGER,
+    id_portafoglio INTEGER,
 
-    CONSTRAINT PK_TRANSAZIONE_ENTRATA PRIMARY KEY (CRO),
-    CONSTRAINT FK_CARTA_CREDITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_CREDITO(NumeroCarta) ON DELETE CASCADE,
-    CONSTRAINT FK_CARTA_DEBITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_DEBITO(NumeroCarta) ON DELETE CASCADE,
-    CONSTRAINT importo CHECK(Importo>0)
+     CONSTRAINT PK_TRANSAZIONE_ENTRATA PRIMARY KEY (CodTransazione),
+    CONSTRAINT FK_CARTA_CREDITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_CREDITO(id_carta) ON DELETE CASCADE,
+    CONSTRAINT FK_CARTA_DEBITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_DEBITO(id_Carta) ON DELETE CASCADE,
+    CONSTRAINT FK_PORTAFOGLIO FOREIGN KEY(id_portafoglio) REFERENCES a.PORTAFOGLIO(id_portafoglio) ON DELETE CASCADE,
+    CONSTRAINT Importo CHECK(Importo>0)
 
 );
 
 CREATE TABLE a.TRANSAZIONE_USCITA(
-    CRO varchar(11),
+    CodTransazione serial,
     Importo float NOT NULL,
     DataTransazione date NOT NULL,
-    Casuale varchar(64),
-    Beneficiario varchar(128) NOT NULL,
-    IBAN_destinatario varchar(27) NOT NULL,
     Categoria varchar(16) NOT NULL,
     CartaDebito INTEGER,
     CartaCredito INTEGER,
+    id_portafoglio INTEGER,
 
-    CONSTRAINT PK_TRANSAZIONE_USCITA PRIMARY KEY (CRO),
-    CONSTRAINT FK_CARTA_CREDITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_CREDITO(NumeroCarta) ON DELETE CASCADE,
-    CONSTRAINT FK_CARTA_DEBITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_DEBITO(NumeroCarta) ON DELETE CASCADE,
-    CONSTRAINT importo CHECK(Importo>0)
-
+    CONSTRAINT PK_TRANSAZIONE_USCITA PRIMARY KEY (CodTransazione),
+    CONSTRAINT FK_PORTAFOGLIO FOREIGN KEY(id_portafoglio) REFERENCES a.PORTAFOGLIO(id_portafoglio) ON DELETE CASCADE,
+    CONSTRAINT FK_CARTA_CREDITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_CREDITO(id_carta) ON DELETE CASCADE,
+    CONSTRAINT FK_CARTA_DEBITO FOREIGN KEY(CartaCredito) REFERENCES a.CARTA_DEBITO(id_Carta) ON DELETE CASCADE,
+    CONSTRAINT Importo CHECK(Importo>0)
 );
+
+--CREATE TABLE a.CONTO(
+    --IBAN varchar(27),
+    --SaldoConto float,
+    --CodF varchar(16),
+
+    --CONSTRAINT PK_Conto PRIMARY KEY (IBAN),
+    --CONSTRAINT FK_Utente FOREIGN KEY(CodF) REFERENCES a.UTENTE(CodF) ON DELETE CASCADE
+--);
+
